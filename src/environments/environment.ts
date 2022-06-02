846cc102153e318354fb12f712df730adb0d2356
Nesrine Sboui <sbouii.nesrine@gmail.com>
2022-06-03 00:18:19 +0200

    install material and firebase package

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'my-budgetapi',
+    appId: '1:406001023195:web:9d7a468fc0cc26b3f174fe',
+    storageBucket: 'my-budgetapi.appspot.com',
+    locationId: 'europe-west2',
+    apiKey: 'AIzaSyDSMsJiI627jZuULL8BKn18V_FFiaeqod0',
+    authDomain: 'my-budgetapi.firebaseapp.com',
+    messagingSenderId: '406001023195',
+    measurementId: 'G-68WJS97HMV',
+  },
   production: false
 };
 
