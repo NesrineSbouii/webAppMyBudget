2c505149df97c35fcb79579d0359d75c5f22eb39
Nesrine Sboui <sbouii.nesrine@gmail.com>
2022-06-03 02:12:09 +0200

    add snackbar error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatSnackBarModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -9,23 +9,33 @@ import {
 
 import { Injectable } from '@angular/core';
 import { LoginData } from '../interfaces/login-data.interface';
+import { SnackBarService } from './snackbar.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private auth: Auth) {}
+  constructor(private auth: Auth, private snackBService: SnackBarService) { }
 
   login({ email, password }: LoginData) {
-    return signInWithEmailAndPassword(this.auth, email, password);
+    return signInWithEmailAndPassword(this.auth, email, password)
+      .catch((error: string) => {
+        this.snackBService.openSnackBar(error, 'X');
+      });;;
   }
 
   loginWithGoogle() {
-    return signInWithPopup(this.auth, new GoogleAuthProvider());
+    return signInWithPopup(this.auth, new GoogleAuthProvider())
+      .catch((error: string) => {
+        this.snackBService.openSnackBar(error, 'X');
+      });;
   }
 
   register({ email, password }: LoginData) {
-    return createUserWithEmailAndPassword(this.auth, email, password);
+    return createUserWithEmailAndPassword(this.auth, email, password)
+      .catch((error: string) => {
+        this.snackBService.openSnackBar(error, 'X');
+      });
   }
 
   logout() {
