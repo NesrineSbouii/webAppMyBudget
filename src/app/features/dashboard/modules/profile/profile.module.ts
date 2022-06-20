f0032a160d115d0801ac6c40424c85a05db09d4e
Nesrine Sboui <sbouii.nesrine@gmail.com>
2022-06-20 03:42:56 +0200

    profile integration

--- src/app/features/dashboard/modules/profile/profile.module.ts
+++ src/app/features/dashboard/modules/profile/profile.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
-
+import { ProfileComponent } from './components/profile.component';
+import { ProfileRoutingModule } from './profile-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core'
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileComponent,
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    ProfileRoutingModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
+  providers: [
+    MatDatepickerModule,
+  ],
 })
 export class ProfileModule { }
