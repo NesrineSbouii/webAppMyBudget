4352c7d359fa151a0f0cf13aa49a9b3a1612d9db
Nesrine Sboui <sbouii.nesrine@gmail.com>
2022-08-25 01:57:27 +0200

    remove project from group form

--- src/app/features/dashboard/modules/groups/components/group-form/group-form.component.ts
+++ src/app/features/dashboard/modules/groups/components/group-form/group-form.component.ts
@@ -10,8 +10,6 @@ import {
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Group } from '../../../models/group';
-import { Project } from '../../../projects/models/project';
-import { ProjectService } from '../../../projects/services/project.service';
 import { User } from '../../../users/models/user';
 import { UserService } from '../../../users/services/user.service';
 import { Router } from '@angular/router';
@@ -29,24 +27,17 @@ export class GroupFormComponent implements OnInit, OnChanges {
 
   form: FormGroup;
   usersList: Observable<User[]>;
-  projects: Project[];
-  filteredProjects: Project[];
   users: User[];
   filteredUsers: User[];
 
   constructor(
     private fb: FormBuilder,
     private userService: UserService,
-    private projectService: ProjectService,
     private router: Router,
   ) {}
 
   get name() {
     return this.form.get('name');
-  }
-
-  get project() {
-    return this.form.get('project');
   }
 
   get members() {
@@ -56,19 +47,9 @@ export class GroupFormComponent implements OnInit, OnChanges {
   ngOnInit(): void {
     this.form = this.fb.group({
       name: ['', [Validators.required]],
-      project: ['', [Validators.required]],
       members: [[], [Validators.required]],
     });
     this.form.patchValue(this.initData);
-
-    this.projectService.list()
-      .subscribe((projects: Project[]) => {
-        this.projects = projects;
-        this.filteredProjects = projects;
-      });
-    this.project?.valueChanges.subscribe((value) => {
-      this.filteredProjects = this._filterProjects(value || '');
-    });
 
     this.usersList = this.userService.list();
   }
@@ -80,13 +61,6 @@ export class GroupFormComponent implements OnInit, OnChanges {
     ) {
       this.form?.patchValue(simpleChanges['initData'].currentValue);
     }
-  }
-
-  private _filterProjects(value: string): Project[] {
-    const filterValue = value.toLowerCase();
-    return this.projects?.filter((option) =>
-      option.name.toLowerCase().includes(filterValue)
-    );
   }
 
   onSubmit():void {
